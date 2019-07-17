f12a48d722ce3c71c12b6650e6b7063050dedd2d
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2019-07-16 23:02:39 -0300

    Acessando o site Pudim

--- Mundo_3_EstruturasCompostas/ex114.py
+++ Mundo_3_EstruturasCompostas/ex114.py
@@ -8,11 +8,11 @@ Resolução do problema:
 """
 # Necessário instalar a biblioteca "requests" para importação
 import requests
-
 try:
     # A variável "response" recebe o objeto Response gerado pela requisição de metodo GET
     url = 'http://www.pudim.com.br/'
     response = requests.get(url)
+
     print(f'\n\33[32m{" CONEXÃO REALIZADA ":-^43}')
     print('Parece que você CONSEGUIU entrar no site...')
     print(f'URL: {url}'.center(43))
